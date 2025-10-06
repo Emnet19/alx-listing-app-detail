ff7f4b74548ad7adfcd5943a1fa7cb8fadf6f7ea
Emnet Befkadu <162457277+Emnet19@users.noreply.github.com>
2025-10-06 07:15:11 +0300

    Update index.ts

--- interface/index.ts
+++ interface/index.ts
@@ -9,3 +9,16 @@ export interface ButtonProps {
   onClick?: () => void
   type?: "button" | "submit" | "reset"
 }
+export interface PropertyProps{
+  name:string;
+  address:{
+    city:string;
+    country:string;
+  }
+  category:string[];
+  rating:string;
+  image:string;
+  description:string;
+  price:number;
+}
+
