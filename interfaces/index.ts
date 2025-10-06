f3152497ec08efd5c1fb62967af4a4369607cdd4
Emnet Befkadu <162457277+Emnet19@users.noreply.github.com>
2025-10-06 07:17:17 +0300

    Create index.ts

---
+++ interfaces/index.ts
@@ -0,0 +1,24 @@
+export interface CardProps {
+  title: string
+  description: string
+  imageUrl: string
+}
+
+export interface ButtonProps {
+  label: string
+  onClick?: () => void
+  type?: "button" | "submit" | "reset"
+}
+
+export interface PropertyProps{
+  name:string;
+  address:{
+    city:string;
+    country:string;
+  }
+  category:string[];
+  rating:string;
+  image:string;
+  description:string;
+  price:number;
+}
