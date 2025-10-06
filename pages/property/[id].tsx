545f13585a0a05a3049998e0c031df8bfb42a843
Emnet Befkadu <162457277+Emnet19@users.noreply.github.com>
2025-10-06 07:12:24 +0300

    Create [id].tsx

---
+++ pages/property/[id].tsx
@@ -0,0 +1,21 @@
+import { PROPERTYLISTINGSAMPLE } from "@/constants/index";
+import { useRouter } from "next/router";
+import PropertyDetail from "@/components/property/PropertyDetail";
+
+export default function PropertyPage() {
+  const router = useRouter();
+//   const { id } = router.query;
+//   const property = PROPERTYLISTINGSAMPLE.find((item) => item.name === id);
+const { id } = router.query;
+const property = PROPERTYLISTINGSAMPLE.find(
+  (item) => item.name === String(id)
+);
+
+  if (!property) return <p>Property not found</p>;
+
+  return (
+    <div>
+      <PropertyDetail property={property} />
+    </div>
+  );
+}
